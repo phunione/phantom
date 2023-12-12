3dc4807083cc0a653657ccfdce6d56461bc8baca
Kushagra <work.guptakush@gmail.com>
2023-12-12 19:40:11 +0530

    remove: node.js backend

--- node_backend/controllers/genericFunctions/duplicateCheck.js
+++
@@ -1,9 +0,0 @@
-const duplicateCheck = async (Model, id, details) => {
-    try {
-        const m = await Model.find(details);
-        return m && id !== m._id;
-    } catch (error) {
-        throw new Error(`Error during duplicate check: ${error.message}`);
-    }
-}
-export default duplicateCheck;
