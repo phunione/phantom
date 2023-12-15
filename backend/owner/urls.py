1f6f4b09838ae187f9e162b6e5b9c753df71861b
phunione <147757277+phunione@users.noreply.github.com>
2023-12-15 17:41:50 +0530

    get one and etlet one in owner

--- backend/owner/urls.py
+++ backend/owner/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('all/', views.getAll, name='get-all-owners'),
-	# path('<id>/', views.getSingle, name='get-single-owner'),
+	path('<id>/', views.getOne, name='get-single-owner'),
+	path('delete/<id>/', views.delete_one, name='delete-single-owner'),
+
 ]
--- backend/owner/views.py
+++ backend/owner/views.py
@@ -19,3 +19,25 @@ def getAll(req):
 			'error': 'Error Fetching Owners'
 		}
 		return Response(message, status=status.HTTP_418_IM_A_TEAPOT)
+@api_view(['GET'])
+def getOne(request, id):
+    try:
+        owner =  Owner.objects.get(id = id)
+        serializer = OwnerSerializer(owner, many = False)
+        return Response(serializer.data,status = status.HTTP_200_OK)
+    except Exception as e:
+        print(e)
+        message = {'success': False, 'error': e}
+        return Response(message, status=status.HTTP_418_IM_A_TEAPOT)
+    
+@api_view(['DELETE'])
+def delete_one(request,id):
+    try:
+        owner = Owner.objects.get(id = id)
+        owner.delete()
+        message = {'success': True, 'message': "Owner deleted successfully"}
+        return Response(message,status = status.HTTP_200_OK)
+    except Exception as e:
+        print(e)
+        message = {'success': False, 'error': e}
+        return Response(message, status=status.HTTP_418_IM_A_TEAPOT)
