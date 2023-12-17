ef576cd88240e1ae0b128734e2a7634c9e97aa05
phunione <147757277+phunione@users.noreply.github.com>
2023-12-17 14:20:47 +0530

    delete route for excel company added

--- backend/ExcelData/urls.py
+++ backend/ExcelData/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	path('add/', views.add, name='add'),
 	path('all/', views.getAll, name='get-all-excelcompanies'),
+	path('delete/<id>/', views.delete, name='delete-company'),
 
 ]
--- backend/ExcelData/views.py
+++ backend/ExcelData/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
-from .serializers import ExcelDataSerializer
+from .serializer import ExcelDataSerializer
 from .models import ExcelCompany
 
 
@@ -68,3 +68,15 @@ def getAll(req):
 		message = {'success': False, 'error': 'Error Fetching excelCompanies'}
 		return Response(message, status=status.HTTP_418_IM_A_TEAPOT)
 
+
+@api_view(['Delete'])
+def delete(request, id):
+	try:
+		excel = ExcelCompany.objects.get(id=id)
+		excel.delete()
+		message = {'success': True, 'message': "excel comapny deleted successfully"}
+		return Response(message, status=status.HTTP_200_OK)
+	except Exception as e:
+		print(e)
+		message = {'success': False, 'error': e}
+		return Response(message, status=status.HTTP_418_IM_A_TEAPOT)
