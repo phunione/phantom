185a522a49f10c591b151a7dbd8e75e5fb02c515
phunione <147757277+phunione@users.noreply.github.com>
2023-12-18 17:39:39 +0530

    fixing size of adhar number and pan numbers

--- backend/actor/models.py
+++ backend/actor/models.py
@@ -8,11 +8,11 @@ from owner.models import Owner
 # Create your models here.
 class Actor(models.Model):
 	name = models.CharField(max_length=200, null=False, blank=False)
-	adhar_number = models.CharField(max_length=14, null=False, blank=False)
-	pan_number = models.CharField(max_length=10, null=False, blank=False)
+	adhar_number = models.CharField(max_length=24, null=False, blank=False)
+	pan_number = models.CharField(max_length=20, null=False, blank=False)
 	din_number = models.CharField(max_length=20, null=True, blank=True)
 	otp_phoneNr = models.CharField(max_length=20, null=True, blank=True)
-	sim_number = models.CharField(max_length=10, null=True, blank=True)
+	sim_number = models.CharField(max_length=20, null=True, blank=True)
 	email = models.EmailField()
 	per_phone = models.CharField(max_length=20, null=True, blank=True)
 	mother_name = models.CharField(max_length=200, null=True, blank=True)
